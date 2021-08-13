a90581358b31e0b13e002df5376a6b89e4152159
mdennis07 <55273352+mdennis07@users.noreply.github.com>
2021-08-13 21:09:44 +0800

    Test.py
    Integer checker Python script

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello world")
+def solution(N):
+    print(N)
+
+def integerChecker(N):
+    print ("This is a non-negative checker function: " + N)
+    if N.isdigit():
+        solution(N)
+    else:
+        #Do Nothing
+
+    integerChecker(250)
+
